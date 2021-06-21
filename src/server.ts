609ed7007142d35af3baf6a9959ab5a3dcb586af
Massaaki <mauricio.massaaki@gmail.com>
2021-06-20 21:04:12 -0300

    feat: Add fiest route GET & POST

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,19 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * GET    => Search for informations
+ * POST   => Insert/create informations
+ * PUT    => change information
+ * DELETE => Remove data
+ * PATCH  => Change specific information
+ */
+app.get('/test', (request, response) => {
+	return response.send('hello nlw!');
+})
+
+app.post('/test-post', (request, response) => {
+	return response.send('Hello nlw post method');
+})
+
 app.listen(3000, () => console.log('Server is running'));
