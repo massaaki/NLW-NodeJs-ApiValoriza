c5a1014e87dcbb74a0d3ca47839fc5b73b82ba82
Massaaki <mauricio.massaaki@gmail.com>
2021-06-22 21:20:16 -0300

    feat: add create tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/createUserController';
+import { CreateTagController } from './controllers/createTagController';
+import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
-router.post("/users/create", createUserController.handle)
+router.post("/users/create", createUserController.handle);
+router.post("/tags/create", ensureAdmin, createTagController.handle);
 
 export { router };
