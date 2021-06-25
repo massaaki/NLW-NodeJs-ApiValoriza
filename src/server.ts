71fbfd7fb9d53d4c60e6c9d3262ad955eb23e011
Massaaki <mauricio.massaaki@gmail.com>
2021-06-24 21:49:28 -0300

    feat: add and configure cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
+import cors from 'cors';
 
 import { AppError } from './errors/AppError';
 
@@ -9,6 +10,8 @@ import { router } from './routes';
 import "./database";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
